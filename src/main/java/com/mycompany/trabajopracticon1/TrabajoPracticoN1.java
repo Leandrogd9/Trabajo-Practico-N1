90f45b44e05ec2b94439e0b19723da59f709b952
Leandrogd9 <leandrogd67@gmail.com>
2023-09-29 16:20:58 -0300

    "En proceso"

---
+++ src/main/java/com/mycompany/trabajopracticon1/TrabajoPracticoN1.java
@@ -0,0 +1,11 @@
+package com.mycompany.trabajopracticon1;
+import IGU.Principal;
+
+public class TrabajoPracticoN1 {
+
+    public static void main(String[] args) {
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
